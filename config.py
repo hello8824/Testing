1b72626d980a9e09fed1bba11b5a6cbdcce219ba
hello8824 <finishermods4@gmail.com>
2026-04-07 20:10:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7308192196:AAG8FqyhottYzyxbC42mbybNmgwl8PNsKLA")
+BOT_TOKEN=("7653519309:AAFt6HrGYFn_UBhuHjMK_O9_9kNzSyp3cqs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("ILLEGALCHEAT78")
+OWNER_USERNAME=("Shout_hu_bro")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("758798248")
+ADMIN_IDS=("844639708")
 
 
 
